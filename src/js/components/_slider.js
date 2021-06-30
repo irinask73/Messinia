1acb6d6a3d703d8b82a934a18d132eaa919e35d4
irinask73 <irinaskid@gmail.com>
2021-06-30 15:15:11 +0300

    clone project

---
+++ src/js/components/_slider.js
@@ -0,0 +1,29 @@
+const slider = new Swiper('.swiper-container', {
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  spaceBeteeen: 0,
+  autoplay: {
+    delay: 5000,
+  },
+  keyboard: {
+    enabled: true,
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+//clients
+const clientsSwiper = new Swiper('.clients__slider', {
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.clients__button-next',
+    prevEl: '.clients__button-prev',
+  },
+});
